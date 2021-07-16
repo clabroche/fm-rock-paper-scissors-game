87c129891647f2ee00a6b0fe933b28a6e5996079
clabroche <corentin.labroche@sailendra.fr>
2021-07-16 17:13:57 +0200

    fix(design): change flex position to grid position

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,45 @@
 <template>
 <div class="app">
-
-  <!-- <div class="root triangle">
-    <img class="bg" src="@/assets/img/bg-triangle.svg" alt="">
-    <div class="buttons">
-      <div class="line space">
-        <div class="button paper">
-          <div>
-            <img src="@/assets/img/icon-paper.svg" alt="">
-          </div>
-        </div>
-        <div class="button scissors">
-          <div>
-            <img src="@/assets/img/icon-scissors.svg" alt="">
-          </div>
-        </div>
-      </div>
-      <div class="line center">
-        <div class="button rock">
-          <div>
-            <img src="@/assets/img/icon-rock.svg" alt="">
-          </div>
-        </div>
-      </div>
-    </div>
-  </div> -->
-  <div class="root pentagon">
+  <div class="header">
+    <h1 v-if="mode==='pentagon'">Rock, paper, scissors, lizard, spock</h1>
+    <h1 v-else>Rock, paper, scissors</h1>
+    <select v-model="mode">
+      <option value="triangle">Rock, paper, scissors</option>
+      <option value="pentagon">Rock, paper, scissors, lizard, spock</option>
+    </select>
+  </div>
+  <div class="root pentagon" v-if="mode === 'pentagon'">
     <img class="bg" src="@/assets/img/bg-pentagon.svg" alt="">
     <div class="buttons">
-      <div class="line center">
-        <div class="button scissors">
-          <div>
-            <img src="@/assets/img/icon-scissors.svg" alt="">
-          </div>
-        </div>
-      </div>
-      <div class="line space">
-        <div class="button spock">
-          <div>
-            <img src="@/assets/img/icon-spock.svg" alt="">
-          </div>
-        </div>
-
-        <div class="button paper">
-          <div>
-            <img src="@/assets/img/icon-paper.svg" alt="">
-          </div>
-        </div>
-      </div>
-      <div class="line space">
-        <div class="button lizard">
-          <div>
-            <img src="@/assets/img/icon-scissors.svg" alt="">
-          </div>
-        </div>
-        <div class="button rock">
-          <div>
-            <img src="@/assets/img/icon-rock.svg" alt="">
-          </div>
-        </div>
-      </div>
+      <button-cmp icon="scissors"/>
+      <button-cmp icon="spock"/>
+      <button-cmp icon="paper"/>
+      <button-cmp icon="lizard"/>
+      <button-cmp icon="rock"/>
+    </div>
+  </div>
+  <div class="root triangle" v-else>
+    <img class="bg" src="@/assets/img/bg-triangle.svg" alt="">
+    <div class="buttons">
+      <button-cmp icon="scissors"/>
+      <button-cmp icon="paper"/>
+      <button-cmp icon="rock"/>
     </div>
   </div>
 </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
+import ButtonCmp from "./components/Button.vue"
 
 export default {
   name: 'App',
-  components: {
+  components: { ButtonCmp },
+  setup() {
+    return {
+      mode: ref('pentagon')
+    }
   }
 }
 </script>
@@ -93,11 +63,19 @@ h2  {font-size: 1.9em;}
 h3  {font-size: 1.8em;}
 h4  {font-size: 1.7em;}
 h5  {font-size: 1.6em;}
-
+.app {
+  display: flex;
+  flex-direction: column;
+  select {
+    margin-bottom: 40px;
+  }
+}
 .root {
   position: relative;
+  padding: 40px;
   .bg {
-    height: 100%;
+    height: 330px;
+    width: 330px;
   }
   .buttons {
     position: absolute;
@@ -105,93 +83,50 @@ h5  {font-size: 1.6em;}
     left: 0;
     width: 100%;
     height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    .line {
-      display: flex;
-      &.space {
-        justify-content: space-between;
-      }
-      &.center {
-        justify-content: center;
-      }
-      .button {
-        background-color: red;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-radius: 50%;
-        padding: 15px;
-        position: relative;
-        z-index: 1;
-        border-bottom: 6px solid black;
-        &:hover {
-          transform: scale(1.1);
-        }
-        &.paper {
-          background: hsl(230, 89%, 62%);
-          background: radial-gradient(circle, hsl(230, 89%, 62%) 0%, hsl(230, 89%, 65%) 100%);
-          border-color: hsl(230, 89%, 55%);
-        }
-        &.scissors {
-          background: hsl(39, 89%, 49%);
-          background: radial-gradient(circle, hsl(39, 89%, 49%) 0%, hsl(40, 84%, 53%) 100%);
-          border-color: hsl(40, 84%, 40%);
-        }
-        &.rock {
-          background: hsl(349, 71%, 52%);
-          background: radial-gradient(circle, hsl(349, 71%, 52%) 0%, hsl(349, 70%, 56%) 100%);
-          border-color: hsl(349, 70%, 40%);
-        }
-        &.lizard {
-          background: hsl(261, 73%, 60%);
-          background: radial-gradient(circle, hsl(261, 73%, 60%) 0%, hsl(261, 72%, 63%) 100%);
-        }
-        &.spock {
-          background: hsl(189, 59%, 53%);
-          background: radial-gradient(circle, hsl(189, 59%, 53%) 0%, hsl(189, 58%, 57%) 100%);
-        }
-        &>div {
-          border-radius: 50%;
-          background-color: #fff;
-          width: 100px;
-          height: 100px;
-          flex-shrink: 0;
-          box-shadow: 0 4px 0  2px #ccc inset;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-      }
-    }
+    display: grid;
   }
   &.triangle {
+    .buttons {
+      grid-template-columns: repeat(8, 1fr);
+      grid-template-rows: repeat(4, 1fr);
+    }
     .paper {
-      transform: translateX(-50%) translateY(-50%);
+      grid-row: 1;
+      grid-column: 1;
     }
     .scissors {
-      transform: translateX(50%) translateY(-50%);
+      grid-row: 1;
+      grid-column: 8;
     }
     .rock {
-      transform: translateY(50%);
+      grid-row: 4;
+      grid-column: 1 / 9
     }
   }
   &.pentagon {
+    .buttons {
+      grid-template-columns: repeat(6, 1fr);
+      grid-template-rows: repeat(4, 1fr);
+    }
     .paper {
-      transform: translateX(50%) translateY(-50%);
+      grid-column: 6;
+      grid-row: 2;
     }
     .scissors {
-      transform: translateY(-50%);
+      grid-row: 1;
+      grid-column: 1 / 7;
     }
     .rock {
-      transform: translateY(-25%);
+      grid-column: 2;
+      grid-row: 4;
     }
     .lizard {
-      transform: translateY(-25%);
+      grid-row: 2;
+      grid-column: 1;
     }
     .spock {
-      transform: translateX(-50%) translateY(-50%);
+      grid-row: 4;
+      grid-column: 5;
     }
   }
 }
---
+++ src/components/Button.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="button-root" :class="icon">
+    <div class="button" :class="icon">
+      <div class="">
+        <img :src="svg[icon]" :alt="'icon-' + icon">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import scissorsSVG from '@/assets/img/icon-scissors.svg'
+import spockSVG from '@/assets/img/icon-spock.svg'
+import paperSVG from '@/assets/img/icon-paper.svg'
+import lizardSVG from '@/assets/img/icon-lizard.svg'
+import rockSVG from '@/assets/img/icon-rock.svg'
+export default {
+  props: {
+    /** @type {'lizard' | 'spock' | 'rock' | 'scissors' | 'paper'} */
+    icon: {default: '', type: String}
+  },
+  setup() {
+    return {
+      svg: {
+        lizard: lizardSVG,
+        spock: spockSVG,
+        paper: paperSVG,
+        rock: rockSVG,
+        scissors: scissorsSVG,
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.button-root {
+  position: relative;
+  width: 100%;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.button {
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  z-index: 1;
+  border-bottom: 6px solid black;
+  width: 120px;
+  height: 120px;
+  transition: 300ms;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.1);
+  }
+  &.paper {
+    background: hsl(230, 89%, 62%);
+    background: radial-gradient(circle, hsl(230, 89%, 62%) 0%, hsl(230, 89%, 65%) 100%);
+    border-color: hsl(230, 89%, 55%);
+  }
+  &.scissors {
+    background: hsl(39, 89%, 49%);
+    background: radial-gradient(circle, hsl(39, 89%, 49%) 0%, hsl(40, 84%, 53%) 100%);
+    border-color: hsl(40, 84%, 40%);
+  }
+  &.rock {
+    background: hsl(349, 71%, 52%);
+    background: radial-gradient(circle, hsl(349, 71%, 52%) 0%, hsl(349, 70%, 56%) 100%);
+    border-color: hsl(349, 70%, 40%);
+  }
+  &.lizard {
+    background: hsl(261, 73%, 60%);
+    background: radial-gradient(circle, hsl(261, 73%, 60%) 0%, hsl(261, 72%, 63%) 100%);
+  }
+  &.spock {
+    background: hsl(189, 59%, 53%);
+    background: radial-gradient(circle, hsl(189, 59%, 53%) 0%, hsl(189, 58%, 57%) 100%);
+  }
+  &>div {
+    border-radius: 50%;
+    background-color: #fff;
+    flex-shrink: 0;
+    box-shadow: 0 4px 0  2px #ccc inset;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
+    img {
+      padding: 15px;
+      width: 40px;
+      height: 40px;
+    }
+  }
+}
+
+</style>
